343cebfe03e121e353e2eeee25ff9ea8b0b4f036
felabel <felicityabel2016@gmail.com>
2024-02-20 07:12:26 +0100

    print fizz

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -3,6 +3,7 @@
 """
 import sys
 
+
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -26,11 +27,12 @@ def fizzbuzz(n):
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
+
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
+    if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 18")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
