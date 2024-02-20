755b697c83bc8d0fccb439cd20573224040743fd
felabel <felicityabel2016@gmail.com>
2024-02-20 06:50:28 +0100

    fizzbuz fix

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -27,7 +27,7 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 18")
